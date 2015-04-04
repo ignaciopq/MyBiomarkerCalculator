4ab195df3ae86d0456110b9d993a517324d2830d
Haonan Xu <haonanxu@Haonans-MacBook-Pro.local>
2015-04-04 12:19:50 -0700

    bubbles added

---
+++ WebWithBubble/vendor/slim/slim/index.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Step 1: Require the Slim Framework
+ *
+ * If you are not using Composer, you need to require the
+ * Slim Framework and register its PSR-0 autoloader.
+ *
+ * If you are using Composer, you can skip this step.
+ */
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+/**
+ * Step 2: Instantiate a Slim application
+ *
+ * This example instantiates a Slim application using
+ * its default settings. However, you will usually configure
+ * your Slim application now by passing an associative array
+ * of setting names and values into the application constructor.
+ */
+$app = new \Slim\Slim();
+
+/**
+ * Step 3: Define the Slim application routes
+ *
+ * Here we define several Slim application routes that respond
+ * to appropriate HTTP request methods. In this example, the second
+ * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
+ * is an anonymous function.
+ */
+
+// GET route
+$app->get('/:var','selectData');
+function selectData($var)
+{
+    // $url = "SELECT age,bisphenol,bloodLead,cotinine,diethylthiophosphate,hexylphthalate,pcb153 FROM medianAge";
+    switch ($var){
+
+        case 1:
+            $qur = "SELECT country as name, bloodLead as size FROM medianCountry";
+            getData($qur,"country","bloodLead");
+            break;
+        case 2:
+            $qur = "SELECT country as name, bisphenol as size FROM medianCountry";
+            getData($qur,"country","bisphenol");
+            break;
+        case 3:
+            $qur = "SELECT country as name, cotinine as size FROM medianCountry";
+            getData($qur,"country","cotinine");
+            break;
+        case 4:
+            $qur = "SELECT country as name, diethylthiophosphate as size FROM medianCountry";
+            getData($qur,"country","diethylthiophosphate");
+            break;
+        case 5:
+            $qur = "SELECT country as name, mercury as size FROM medianCountry";
+            getData($qur,"country","mercury");
+            break;
+        case 6:
+            $qur = "SELECT country as name, hexylphthalate as size FROM medianCountry";
+            getData($qur,"country","hexylphthalate");
+            break;
+        case 7:
+            $qur = "SELECT country as name, pcb153 as size FROM medianCountry";
+            getData($qur,"country","pcb153");
+            break;
+        case 8:
+            $qur = "SELECT age as name, bloodLead as size FROM medianAge";
+            getData($qur,"age","bloodLead");
+            break;
+        case 9:
+            $qur = "SELECT age as name, bisphenol as size FROM medianAge";
+            getData($qur,"age","bisphenol");
+            break;
+        case 10:
+            $qur = "SELECT age as name, cotinine as size FROM medianAge";
+            getData($qur,"age","cotinine");
+            break;
+        case 11:
+            $qur = "SELECT age as name, diethylthiophosphate as size FROM medianAge";
+            getData($qur,"age","diethylthiophosphate");
+            break;
+        case 12:
+            $qur = "SELECT age as name, mercury as size FROM medianAge";
+            getData($qur,"age","mercury");
+            break;
+        case 13:
+            $qur = "SELECT age as name, hexylphthalate as size FROM medianAge";
+            getData($qur,"age","hexylphthalate");
+            break;
+        case 14:
+            $qur = "SELECT age as name, pcb153 as size FROM medianAge";
+            getData($qur,"age","pcb153");
+            break;
+        case 15:
+            $qur = "SELECT gender as name, bloodLead as size FROM medianGender";
+            getData($qur,"gender","bloodLead");
+            break;
+        case 16:
+            $qur = "SELECT gender as name, bisphenol as size FROM medianGender";
+            getData($qur,"gender","bisphenol");
+            break;
+        case 17:
+            $qur = "SELECT gender as name, cotinine as size FROM medianGender";
+            getData($qur,"gender","cotinine");
+            break;
+        case 18:
+            $qur = "SELECT gender as name, diethylthiophosphate as size FROM medianGender";
+            getData($qur,"gender","diethylthiophosphate");
+            break;
+        case 19:
+            $qur = "SELECT gender as name, mercury as size FROM medianGender";
+            getData($qur,"gender","mercury");
+            break;
+        case 20:
+            $qur = "SELECT gender as name, hexylphthalate as size FROM medianGender";
+            getData($qur,"gender","hexylphthalate");
+            break;
+        case 21:
+            $qur = "SELECT gender as name, pcb153 as size FROM medianGender";
+            getData($qur,"gender","pcb153");
+            break;
+        case 22:
+            $qur = "SELECT income as name, bloodLead as size FROM medianIncome";
+            getData($qur,"income","bloodLead");
+            break;
+        case 23:
+            $qur = "SELECT income as name, bisphenol as size FROM medianIncome";
+            getData($qur,"income","bisphenol");
+            break;
+        case 24:
+            $qur = "SELECT income as name, cotinine as size FROM medianIncome";
+            getData($qur,"income","cotinine");
+            break;
+        case 25:
+            $qur = "SELECT income as name, diethylthiophosphate as size FROM medianIncome";
+            getData($qur,"income","diethylthiophosphate");
+            break;
+        case 26:
+            $qur = "SELECT income as name, mercury as size FROM medianIncome";
+            getData($qur,"income","mercury");
+            break;
+        case 27:
+            $qur = "SELECT income as name, hexylphthalate as size FROM medianIncome";
+            getData($qur,"income","hexylphthalate");
+            break;
+        case 28:
+            $qur = "SELECT income as name, pcb153 as size FROM medianIncome";
+            getData($qur,"income","pcb153");
+            break;
+        case 29:
+            $qur = "SELECT education as name, bloodLead as size FROM medianEducation";
+            getData($qur,"education","bloodLead");
+            break;
+        case 30:
+            $qur = "SELECT education as name, bisphenol as size FROM medianEducation";
+            getData($qur,"education","bisphenol");
+            break;
+        case 31:
+            $qur = "SELECT education as name, cotinine as size FROM medianEducation";
+            getData($qur,"education","cotinine");
+            break;
+        case 32:
+            $qur = "SELECT education as name, diethylthiophosphate as size FROM medianEducation";
+            getData($qur,"education","diethylthiophosphate");
+            break;
+        case 33:
+            $qur = "SELECT education as name, mercury as size FROM medianEducation";
+            getData($qur,"education","mercury");
+            break;
+        case 34:
+            $qur = "SELECT education as name, hexylphthalate as size FROM medianEducation";
+            getData($qur,"education","hexylphthalate");
+            break;
+        case 35:
+            $qur = "SELECT education as name, pcb153 as size FROM medianEducation";
+            getData($qur,"education","pcb153");
+            break;
+        case 36:
+            $qur = "SELECT race as name, bloodLead as size FROM medianRace";
+            getData($qur,"race","bloodLead");
+            break;
+        case 37:
+            $qur = "SELECT race as name, bisphenol as size FROM medianRace";
+            getData($qur,"race","bisphenol");
+            break;
+        case 38:
+            $qur = "SELECT race as name, cotinine as size FROM medianRace";
+            getData($qur,"race","cotinine");
+            break;
+        case 39:
+            $qur = "SELECT race as name, diethylthiophosphate as size FROM medianRace";
+            getData($qur,"race","diethylthiophosphate");
+            break;
+        case 40:
+            $qur = "SELECT race as name, mercury as size FROM medianRace";
+            getData($qur,"race","mercury");
+            break;
+        case 41:
+            $qur = "SELECT race as name, hexylphthalate as size FROM medianRace";
+            getData($qur,"race","hexylphthalate");
+            break;
+        case 42:
+            $qur = "SELECT race as name, pcb153 as size FROM medianRace";
+            getData($qur,"race","pcb153");
+            break;
+        case 43:
+            $qur = "SELECT smoking as name, bisphenol as size FROM medianSmoking";
+            getData($qur,"smoking","bisphenol");
+            break;
+        case 44:
+            $qur = "SELECT smoking as name, cotinine as size FROM medianSmoking";
+            getData($qur,"smoking","cotinine");
+            break;
+        case 45:
+            $qur = "SELECT smoking as name, diethylthiophosphate as size FROM medianSmoking";
+            getData($qur,"smoking","diethylthiophosphate");
+            break;
+        case 46:
+            $qur = "SELECT smoking as name, hexylphthalate as size FROM medianSmoking";
+            getData($qur,"smoking","hexylphthalate");
+            break;
+    }
+}
+
+function getData($var,$type1,$type2){
+    $db = getDB();
+    $rows = $db->query($var);
+    $data = $rows->fetchAll(PDO::FETCH_OBJ);
+    echo '{"name":"'.$type1.'","children":[{"name":"'.$type2.'","children":'.json_encode($data).'}]}';
+}
+
+function getDB(){
+    $dsn = "mysql:dbname=pollutionDB;host=173.194.255.252";
+    $username = "dbuser";
+    $password = "passw0rd123";
+    $dbConnection = new PDO($dsn, $username, $password);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnection;
+}
+// POST route
+$app->post(
+    '/post',
+    function () use ($app){
+        $array = $app->request->post('questions');
+        echo $array['age'];
+    }
+);
+
+// PUT route
+$app->put(
+    '/put',
+    function () {
+        echo 'This is a PUT route';
+    }
+);
+
+// PATCH route
+$app->patch('/patch', function () {
+    echo 'This is a PATCH route';
+});
+
+// DELETE route
+$app->delete(
+    '/delete',
+    function () {
+        echo 'This is a DELETE route';
+    }
+);
+/**
+ * Step 4: Run the Slim application
+ *
+ * This method should be called last. This executes the Slim application
+ * and returns the HTTP response to the HTTP client.
+ */
+$app->run();
---
+++ WebWithBubble/vendor/slim/slim/js/gb.js
@@ -0,0 +1,207 @@
+
+var finalRoot = JSON.parse('{"name":"result","children":[]}');
+
+var finalCountryRoot = JSON.parse('{"name":"Country","children":[]}');
+var finalAgeRoot = JSON.parse('{"name":"Age","children":[]}');
+var finalIncomeRoot = JSON.parse('{"name":"Income","children":[]}');
+var finalGenderRoot = JSON.parse('{"name":"Gender","children":[]}');
+var finalEducationRoot = JSON.parse('{"name":"Education","children":[]}');
+var finalSmokeRoot = JSON.parse('{"name":"Smoking","children":[]}');
+var finalRaceRoot = JSON.parse('{"name":"Race","children":[]}');
+var finalLandUSRoot = JSON.parse('{"name":"US","children":[]}');
+
+function loadData()
+{
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/1"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/2"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/3"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/4"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/5"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/6"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/7"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/8"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/9"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/10"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/11"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/12"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/13"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/14"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/15"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/16"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/17"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/18"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/19"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/20"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/21"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/22"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/23"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/24"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/25"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/26"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/27"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/28"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/29"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/30"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/31"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/32"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/33"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/34"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/35"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/36"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/37"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/38"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/39"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/40"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/41"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/42"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/43"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/44"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/45"));
+  // mergeData(sendUrl("http://localhost/generalbubble/vendor/slim/slim/index.php/46"));
+  // finalRoot.children.push(finalCountryRoot);
+  // finalRoot.children.push(finalAgeRoot);
+  // finalRoot.children.push(finalGenderRoot);
+  // finalRoot.children.push(finalIncomeRoot);
+  // finalRoot.children.push(finalEducationRoot);
+  // finalRoot.children.push(finalRaceRoot);
+  // finalRoot.children.push(finalSmokeRoot);
+  // showbubble(finalRoot);
+  // var userParam;
+  $.getScript("js/calculator.js", function(){
+    // userParam = getQuestions();
+    console.log(questions["age"]);
+  });
+  // console.log(userParam["age"]);
+}
+
+function sendUrl(urlString){
+  return JSON.parse($.ajax({
+            type: "GET",
+            url: urlString,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            async: false,
+            error: function (msg){
+              alert("Faild: " + msg.status + ": " + msg.statusText);
+          }
+        }).responseText);  
+}
+
+function mergeData(tempRoot){
+
+  switch(tempRoot.name){
+
+    case "country":
+      for(var k in tempRoot.children){
+        finalCountryRoot.children.push(tempRoot.children[k]);
+      }
+      // console.log(JSON.stringify(finalCountryRoot));
+      break;
+    case "age":
+      for(var k in tempRoot.children){
+        finalAgeRoot.children.push(tempRoot.children[k]);
+      }
+      // console.log(JSON.stringify(finalCountryRoot));
+      break;
+    case "gender":
+      for(var k in tempRoot.children){
+        finalGenderRoot.children.push(tempRoot.children[k]);
+      }
+      break;
+    case "income":
+      for(var k in tempRoot.children){
+        finalIncomeRoot.children.push(tempRoot.children[k]);
+      }
+      break;
+    case "education":
+      for(var k in tempRoot.children){
+        finalEducationRoot.children.push(tempRoot.children[k]);
+      }
+      break;
+    case "race":
+      for(var k in tempRoot.children){
+        finalRaceRoot.children.push(tempRoot.children[k]);
+      }
+      break;
+    case "smoking":
+      for(var k in tempRoot.children){
+        finalSmokeRoot.children.push(tempRoot.children[k]);
+      }
+      break;
+  }
+}
+
+function showbubble(root){
+
+var margin = 20,
+    diameter = 960;
+
+var color = d3.scale.linear()
+    .domain([-1, 5])
+    .range(["hsl(264,3%,69%)", "hsl(250,19%,24%)"])
+    .interpolate(d3.interpolateHcl);
+
+var pack = d3.layout.pack()
+    .padding(2)
+    .size([diameter - margin, diameter - margin])
+    .value(function(d) { return d.size; })
+//$(function(){var svg = d3.select("#chart").append("svg:svg");});
+var svg = d3.select("#big-chart").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+  .append("g")
+    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+  var focus = root,
+      nodes = pack.nodes(root),
+      view;
+
+  var circle = svg.selectAll("circle")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+  var text = svg.selectAll("text")
+      .data(nodes)
+    .enter().append("text")
+      .attr("class", "label")
+      .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+      .style("display", function(d) { return d.parent === root ? null : "none"; })
+      .text(function(d) { return d.parent === root ? d.name : d.size ? d.name +" : "+ d.size : d.name; });
+
+  var node = svg.selectAll("circle,text");
+
+  d3.select("#big-chart")
+      .style("background", "transparent")
+      .on("click", function() { zoom(root); });
+
+  zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+  function zoom(d) {
+    var focus0 = focus; focus = d;
+
+    var transition = d3.transition()
+        .duration(d3.event.altKey ? 7500 : 750)
+        .tween("zoom", function(d) {
+          var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+          return function(t) { zoomTo(i(t)); };
+        });
+
+    transition.selectAll("text")
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+        .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+        .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+        .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function(d) { return d.r * k; });
+  }
+
+  d3.select(self.frameElement).style("height", diameter + "px");
+}
+loadData();
